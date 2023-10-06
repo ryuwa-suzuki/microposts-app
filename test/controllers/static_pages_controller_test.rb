ec67d06d15a236a4811a716f1134bb04c2ff7117
ryuwa <ryuusuzuki243@gmail.com>
2023-10-06 12:42:41 +0900

    change: home画面をルートに設定

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,8 +5,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
